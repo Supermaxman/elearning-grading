b048c26d6bbdd8a26d320d5854a2232438a1582d
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-02-24 11:10:24 -0600

    Refined generator.py code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 _PATH_ROOT = os.path.dirname(__file__)
 
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 # python setup.py sdist
 # twine upload .\dist\elearning-grading-VERSION.tar.gz
 HOMEPAGE = "https://github.com/Supermaxman/elearning-grading"
